3e55919d0c80b51925e454d66540ebff2908e663
Colin Miao <sddzok@163.com>
2018-04-18 15:56:46 +0800

    first commit

---
+++ src/learning/Questions.java
@@ -0,0 +1,15 @@
+package learning;
+
+import java.util.HashMap;
+
+public class Questions {
+	private HashMap<String, String> questionMap;
+
+	public HashMap<String, String> getQuestionMap() {
+		return questionMap;
+	}
+
+	public void setQuestionMap(HashMap<String, String> questionMap) {
+		this.questionMap = questionMap;
+	}
+}
